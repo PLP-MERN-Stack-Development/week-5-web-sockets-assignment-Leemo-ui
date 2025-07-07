a1b8f5f4a8f2e8113a424aac0754ce63898fd7a0
Leemo-ui <merkitjohn@gmail.com>
2025-07-07 13:12:37 +0300

    updated a  port number

--- chat-app/server/server.js
+++ chat-app/server/server.js
@@ -227,7 +227,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5175;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`WebSocket endpoint: ws://localhost:${PORT}`);
